6bdf4904cb1b6291f3706d1a767249ccbdff93b3
seblavenant <sebastien@lavenant.me>
2015-03-28 11:39:28 +0100

    feat: initialize security in application

--- src/AbstractApplication.php
+++ src/AbstractApplication.php
@@ -6,6 +6,8 @@ use Puzzle\Configuration;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Spear\Silex\Application\Traits;
+use Silex\Provider\SecurityServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
 
 abstract class AbstractApplication extends \Silex\Application
 {
@@ -19,6 +21,7 @@ abstract class AbstractApplication extends \Silex\Application
         $this->loadConfiguration($configuration);
         $this->enableDebug();
         $this->initializePaths($rootDir);
+        $this->initializeSecurity();
 
         $this->register(new ServiceControllerServiceProvider());
         $this->registerProviders();
@@ -39,6 +42,27 @@ abstract class AbstractApplication extends \Silex\Application
         $this['root.path'] = $this->enforceEndingSlash($rootDir);
         $this['documentRoot.path'] = $this['root.path'] . 'www' . DIRECTORY_SEPARATOR;
         $this['var.path'] = $this['root.path'] . $this->removeWrappingSlashes($this['configuration']->readRequired('app/var.path')) . DIRECTORY_SEPARATOR;
+    }
+
+    private function initializeSecurity()
+    {
+        $this->register(new SecurityServiceProvider());
+        $this['security.firewalls'] = array(
+            'admin' => array(
+                'pattern' => '^/admin',
+                'form' => array('login_path' => '/user/login', 'check_path' => '/admin/login_check'),
+                'users' => array(
+                    'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+                ),
+            ),
+        );
+
+        $this->get('/user/login', function(Request $request){
+            return $this['twig']->render('user/login_form.html.twig', array(
+                    'error'         => $this['security.last_error']($request),
+                    'last_username' => $this['session']->get('_security.last_username'),
+            ));
+        })->bind('user_login');
     }
 
     private function enableDebug()
